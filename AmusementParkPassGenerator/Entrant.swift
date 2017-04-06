6feca89d1b8986320b4df68981999a92974238f8
Nathaniel Remy <nathanielremy1@gmail.com>
2017-04-06 09:03:45 -0300

    Initial commit

---
+++ AmusementParkPassGenerator/Entrant.swift
@@ -0,0 +1,31 @@
+//
+//  Entrant.swift
+//  AmusementParkPassGenerator
+//
+//  Created by Nathaniel Remy on 2017-04-06.
+//  Copyright © 2017 Nathaniel Remy. All rights reserved.
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
